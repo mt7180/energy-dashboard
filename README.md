c6b39cb91edffe8b9751bf7a9799e6a70e0f565d
Mira <mira.theidel@gmx.de>
2024-01-23 12:13:59 +0100

    prepare prefect workflow

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # energy-dashboard
+
+This is a project to visualize real time electricity generation data  for the pan-European market obtained from 
+[ENTSOE](https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html)
+
+
+
+> Note:
+A Prefect Webhook with the follofing template needs to be created to support the energy report email functionality of the app. The url endpoint needs to be saved in a Prefect String block to not be exposed publicly.
+```
+{
+    "event": "email_webhook.called",
+    "resource": {
+        "prefect.resource.id": "email-webhook-id",
+        "message_txt": "{{ body.message_text }}",
+        "message_address": "{{ body.message_address }}"
+    }
+}
+``````
+
+
+### Future Improvements: 
+- include transportation and consumption information
+- include EPEX Spot Dayahead 
+- include current wind speed (on x m height for given geo-loc -> find geo loc of ip address?!)
+- include sun radiation ^
--- prefect_workflows/trigger_webhook.py
+++ prefect_workflows/trigger_webhook.py
@@ -1,16 +1,36 @@
 import requests
 from prefect.blocks.system import String
 
-def trigger_webhook(msg: str) -> None:
-    
 
-    url = 'https://api.prefect.cloud/hooks/bSHnwyVlSn1WfbuRVZH35g'
-    payload = {'message_text': msg}
+def trigger_webhook(payload: dict) -> None:
+    """triggers a prefect webhooks' url endpoint with some payload"""
+    url = String.load("email-webhook-url").value
 
-    x = requests.post(url, json = payload)
+    if not url:
+        raise ValueError("Webhook URL not found")
 
-    print(x)
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 204:
+        raise Exception(
+            f"Failed to call the webhook. Status code: {response.status_code}"
+        )
+
+    print("Successfully called the webhook.")
+
+
+def send_report(email_address: str, email_txt: str) -> None:
+    """send report via email to given email_address by triggering
+    a webhook, which triggers an automated flow run on Prefect Cloud
+    """
+    payload = {"message_address": email_address, "message_text": email_txt}
+    trigger_webhook(payload)
+
 
 if __name__ == "__main__":
     email_address = String.load("test-email").value
-    trigger_webhook(email_address)
+    payload = {
+        "message_address": email_address,
+        "message_text": "Hi from Energy Dashboard",
+    }
+    trigger_webhook(payload)
